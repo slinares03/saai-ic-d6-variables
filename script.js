9eabbea739d8724284a802f821a7ab172cc19029
naaltunian <naaltunian@icloud.com>
2024-05-07 07:22:16 -0400

    solution

---
+++ script.js
@@ -0,0 +1,16 @@
+// all code for this lab to be completed in this file
+
+console.log('start')
+
+console.log('hello')
+
+console.log('animal')
+
+const myFavoriteAnimal = 'dog'
+console.log(myFavoriteAnimal)
+
+const myName = 'Bruce Wayne'
+const age = 35
+const location = 'Gotham City'
+const occupation = 'Batman'
+const crimeFighter = true

f9a4d045a10a2951ba6453c0f33f00842164b88e
Salvador Linares <35157273+slinares03@users.noreply.github.com>
2024-11-08 04:09:39 +0000

    Added js code

---
+++ script.js
@@ -0,0 +1,22 @@
+let animal = "siberian tiger"
+
+console.log("It's Chava")
+
+console.log("I'm Chava frfr")
+
+console.log(animal)
+
+console.log("I'M HEREEE")
+
+//Try It yourself
+//Declare 3 different data types
+//String
+let Name = "Salvador"
+//Comma
+let comma = ","
+//Birth Year
+let number = 2003
+//Job in tech?
+let job = false 
+
+console.log("Name:",Name,comma,"Birth Year:",number,comma,"Has a job in tech:",job)
